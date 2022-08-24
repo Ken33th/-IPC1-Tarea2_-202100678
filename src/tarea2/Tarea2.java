4e1d0ce0e0393a13cc07723fe365f86ccb5531b2
Kenneth Aquino <kennethaquino33@gmail.com>
2022-08-24 11:12:22 -0600

    login

---
+++ src/tarea2/Tarea2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea2;
+
+/**
+ *
+ * @author Kenneth Aquino
+ */
+public class Tarea2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login iniciar = new login();
+        iniciar.setVisible(true);
+        
+    }
+    
+}
